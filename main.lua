78d3a81c32c2f466d7c2b7f8e3ecf0a24025df31
mao06 <scrawlmin@gmail.com>
2022-04-06 10:34:31 -0600

    move main.lua -> src

--- main.lua
+++
@@ -1,5 +0,0 @@
--- main.lua
--- Coltrane Willsey
--- 2022-04-06 [09:49]
-
-require('src/init')
--- src/init.lua
+++
@@ -1,9 +0,0 @@
--- init.lua
--- Coltrane Willsey
--- 2022-04-06 [10:27]
-
---[[
-    -- TODO: Add documentation
---]]
-
-local util = './util/'
